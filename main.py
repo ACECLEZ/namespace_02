f8ce6999ecf1ad1892b0b8f3992bd6339b6c40c9
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2024-06-29 07:32:19 +0000

    format

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def wave():
-  print("hi")
+    return "hi"
 
 @app.route('/animal/<noise>')
 def which_animal(noise):
-  if noise == 'meow':
-    return 'cat'
-  elif noise == 'woof':
-    return 'dog'
-  else:
-    return 'unknown'
-  
-app.run(debug=True, port=5500)
+    if noise == 'meow':
+        return 'cat'
+    elif noise == 'woof':
+        return 'dog'
+    else:
+        return 'unknown'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
