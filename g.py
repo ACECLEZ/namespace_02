b246d51c8a3cbc183098ef36e02e527423021af9
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2024-06-29 07:54:25 +0000

    format

---
+++ g.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def wave():
+    return "hi"
+
+@app.route('/animal/<noise>')
+def which_animal(noise):
+    if noise == 'meow':
+        return 'cat'
+    elif noise == 'woof':
+        return 'dog'
+    else:
+        return 'unknown'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-from flask import Flask
+from flask import Flask, Response
+import json
+import http
 
 app = Flask(__name__)
 
-@app.route('/')
-def wave():
-    return "hi"
+# Route 1 'weapon/all'
+@app.route('/weapon/all')
+def get_all_weapons():
+  with open('weapons.json') as f:
+    data = json.load(f)
+  return data
 
-@app.route('/animal/<noise>')
-def which_animal(noise):
-    if noise == 'meow':
-        return 'cat'
-    elif noise == 'woof':
-        return 'dog'
-    else:
-        return 'unknown'
+# Route 2 'weapon/<item_id>'
+@app.route('/weapon/<item_id>')
+def get_weapon_by_id(item_id):
+  with open('weapon.json') as f:
+    data = json.load(f)
 
+  for weapon_name in data:
+    if data[weapon_name]['item_id'] == int(item_id):
+      return json.dumps(data[weapon_name])
+#  return {'error':'No weapon with requested ID was found'}
+  return Response("{'error':'No weapon with requested ID was found'}", status = http.HTTPSTATUS.NOT_FOUND)
+
+# Route 3 'armour/all'
+
+# Route 4 'armour/<item_id>'
+
+# Route 5 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+  app.run(debug=True,port=3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
